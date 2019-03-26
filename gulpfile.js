deaef1902a36276d568b58ea1c91cb3cf76b9127
hackingjack <hackjack@tutanota.com>
2019-03-26 11:53:27 +0100

    trigger deploy on netlify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 
+
+
 gulp.task('minify', () => {
 //   return gulp.src('public/*.html')
    return gulp.src(['public/*.html','public/*.xml' ])
